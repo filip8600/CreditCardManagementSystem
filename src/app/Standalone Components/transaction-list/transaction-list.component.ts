3f24bd41923b17507a66cc6b3182f8f8149584f8
Filip <f-r-c@LAPTOP-BO73C4EV>
2022-10-03 11:19:38 +0200

    remove button in transactions

--- src/app/Standalone Components/transaction-list/transaction-list.component.ts
+++ src/app/Standalone Components/transaction-list/transaction-list.component.ts
@@ -23,6 +23,10 @@ export class TransactionListComponent implements OnInit {
     return date1.toDateString()
   }
 
+  removeTransaction(uid:string):void{
+    this.ts.removeTransaction(uid).subscribe(()=>this.transactions$=this.ts.getTransactions());
+  }
+
   ngOnInit(): void {
   }
 
