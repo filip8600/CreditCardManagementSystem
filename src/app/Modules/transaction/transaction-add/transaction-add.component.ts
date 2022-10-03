6bbf605dd3a50a7944e503ac32fa6235bcb230d0
AlexanderWodstrup <Alexander.f.wodstrup@gmail.com>
2022-10-03 12:12:12 +0200

    transaction add WIP

--- src/app/Modules/transaction/transaction-add/transaction-add.component.ts
+++ src/app/Modules/transaction/transaction-add/transaction-add.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import {
+  CreditCard,
+  CreditCardService,
+} from 'src/app/Services/credit-card.service';
 
 @Component({
   selector: 'app-transaction-add',
   templateUrl: './transaction-add.component.html',
-  styleUrls: ['./transaction-add.component.scss']
+  styleUrls: ['./transaction-add.component.scss'],
 })
 export class TransactionAddComponent implements OnInit {
+  constructor(private service: CreditCardService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
